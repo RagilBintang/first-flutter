0468c2d26539d178f543537570ebe2661619a31f
RagilBintang <ragilbintangbintang@gmail.com>
2025-09-17 14:28:51 +0700

    Belajar Tipe Data

---
+++ 3_type_data.dart
@@ -0,0 +1,53 @@
+/**
+ * Tipe data adalah suatu konsep yang digunakan
+ * untuk mengklasifikasikan jenis data
+ * yang dapata diproses oleh program
+ *  Jenis Tipe Data
+ */
+
+/** 
+ * int => integer => bilangan bulat
+ * => 1, 2, 3, 4, -1, -2, -3
+ */
+
+/**
+ * double => bilangan desimal
+ * => 3.14, -0.1, 2,5 dsb
+ */
+
+/**
+ * num => gabungan dari integer dan double
+ * => 3.14, 2, -2, 0, 3,15 dst
+ */
+
+/**
+ * bool = bolean => true/false
+ */
+
+/**
+ * String => "kumpulan karakter"
+ * => "Hello", 'World', "1234" dst
+ */
+
+/**
+ * List => berupa daftar
+ * [1,2,3,4,5,6,7,0,-2]
+ * [a,b,c,d,e,f,g]
+ */
+
+/**
+ * Map => berupa key:Valeu
+ */
+
+/**
+ * Dynamic => campuran / Tipe apaun
+ * [1, 'a' {"nama_kota" : "Jakarta"}]
+ */
+void main(List<String> args) {
+  String nama = "Bintang"; //String
+  int umur = 15; //integer
+
+  var tinggi_badan; //Dynamic
+  tinggi_badan = 162;
+  tinggi_badanN = "Satu enam dua";
+}
