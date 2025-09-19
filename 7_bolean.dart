6553911ef02b7914b7cf3fbefe31d5809cc74a32
RagilBintang <ragilbintangbintang@gmail.com>
2025-09-19 10:31:10 +0700

    Belajar Bolean

---
+++ 7_bolean.dart
@@ -0,0 +1,17 @@
+void main() {
+  /**
+ * Simbol ! menandakan note atau bukan
+ */
+
+  /**
+ * tipe data bolean hanya ada 2 nilai
+ * true dan false
+ */
+  var isTrue = true;
+
+  print(isTrue); // true
+  print(!isTrue);
+  print(true != false);
+}
+
+
