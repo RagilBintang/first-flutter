ef5ff293344c041cedfa94b9ea2309d2611cd2ae
RagilBintang <ragilbintangbintang@gmail.com>
2025-10-03 14:16:54 +0700

    Belajar Collection List

---
+++ 22_Collection.dart
@@ -0,0 +1,10 @@
+void main () {
+  /**
+   * List => Berurutan, dibaca dari index 0
+   * Set => Acak
+   * Map => Key Value, dibaca dari key
+   * 
+   * Collection adalah struktur data yang digunakan
+   * menyipan banyak nilai sekaligus.
+   */
+}
