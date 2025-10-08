2970bc6d30050010a0756a3beb70d34119bb3341
RagilBintang <ragilbintangbintang@gmail.com>
2025-10-08 13:33:17 +0700

    Belajar contraktor

--- oop_1/29_construktor.dart
+++ oop_1/29_construktor.dart
@@ -1,14 +1,8 @@
 class Hewan {
-
-/**
- * ketika suatu objek dibuat semua property yang ada class
- * harus memiliki nilai, dan juga kita dapat
- */
   //property
   String? name;
   String? color;
   double? weight;
-
 
   //construktor
 
@@ -17,9 +11,14 @@ class Hewan {
   // Hewan(String name, String color, double weight) {
   //   this.name = name;
   //   this.color = color;
-  //   this.weight = weight;
+  //   this.weight = weight = 12.0;
   // }
+  // named construktor
+  // Hewan.name(this.name);
+  // Hewan.color(this.name);
+  // Hewan.weight(this.name);
 
+  // inisialize list
 
   //method
   void eat() {
@@ -30,3 +29,30 @@ class Hewan {
     print('$name Kalau malam tidur');
   }
 }
+
+/**
+ * ketika sebuah objek dibuat semua property yang ada 
+ * pada class harus memiliki nilai
+ * 
+ * Kita dapat menginisialisasi pada property
+ * atau melalui construktor nya
+ * 
+ * contruktor adalah fungsi special dari sebuah class
+ * yang digunakan untuk membuat objek
+ * 
+ * sesuai dengan namanya digunakan untuk mengtruksir
+ * objek baru
+ * 
+ * apabedanya dengan fungsi lain pada class?
+ * 1. contruktor memiliki nama yang sama dengan class
+ * 2. tidak memiliki nilai kembalian (retrun value)
+ * 3. akan secara otomatis dipanggi; ketika objek dibuat
+ * 
+ * kalau tidak, mendefinisikan contruktor maka
+ * construktor tanpa ragumen akan dibuat.
+ * 
+ * dengan membuat construktor
+ * kita tidak hanya bisa menginisialisasi nilai
+ * namu juta menjalaknan intruksi tertentu ketika
+ * objek dibuat
+ */
