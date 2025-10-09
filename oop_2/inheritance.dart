62e5952635ccfefbd958cd0673c350325df2ca35
RagilBintang <ragilbintangbintang@gmail.com>
2025-10-09 14:14:29 +0700

    Belajar inheritance

---
+++ oop_2/inheritance.dart
@@ -0,0 +1,25 @@
+void main () {
+  /**
+   * inheritance adalah kemampuan 
+   * suatu program untuk membuat class baru dari
+   * kelas kelas yang ada.
+   * 
+   * kensep itu seperti anak mewarisi sifat dari 
+   * orangtua nya.
+   * 
+   * didalam oop yang menurunkan sifat disebut
+   * kelas induk (parents class / super class)
+   * 
+   * kelas yang mewarisi disebut sebagai
+   * kelas anak (child class / sub class) 
+   */
+
+  /**
+   * fish extends hewan
+   * - skinColor // propetis
+   * - swim // method
+   * bird
+   * - featherColore // propetis
+   * - fly() // method
+   */
+}
